4e483db960c616843756ab9c101195aac4510fda
John Lockwood <john@codesolid.com>
2024-03-07 16:22:43 -0500

    Update Jupyter page with plot for a SymPy expression

--- symplot.py
+++ symplot.py
@@ -5,7 +5,6 @@ import sympy.geometry as geo
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def get_plotter(obj):
 	if isinstance(obj, geo.Polygon):
 		return plot_polygon
@@ -30,15 +29,12 @@ def plot_expression(obj, axes, **kwargs):
 	axes.set_ylim(-10, 10)
 	axes.plot(x_vals, y_vals)
 
-
 def plot_circle(obj, axes, **kwargs):
 	circ = obj
 	
 	theta = np.linspace(0, 2*np.pi, 100)
-	
 	x = circ.radius * np.cos(theta) + circ.center.x
 	y = circ.radius * np.sin(theta) + circ.center.y
-	
 	axes.plot(x, y, color="blue", linewidth=.7)
 
 
@@ -46,14 +42,13 @@ def plot_polygon(obj, axes, **kwargs):
 	plot_segments(obj.sides, axes, **kwargs)
 	
 def plot(obj, axes=None, **kwargs):
-	if axes == None:
-		axes = get_default_axes()
 	plotter = get_plotter(obj)
 	if not plotter:
 		print("Warning, unsupported type:", str(type(obj)))
-		return axes
-	else:
-		plotter(obj, axes, **kwargs)
+		return None
+	if axes == None:
+		axes = get_default_axes()
+	plotter(obj, axes, **kwargs)
 	return axes
 
 def make_plottable(p1, p2):
@@ -64,12 +59,3 @@ def plot_segments(segments, axes, **kwargs):
 	for s in segments:
 		x,y = make_plottable(*s.points)
 		axes.plot(x, y, **kwargs)
-
-def test_sympy_expression():
-	import sympy as sp
-	
-	x = sp.symbols('x')
-	f = x**3 - x/2
-	ax = plot(f)
-	print("plot_expression successfully returned a(n)", type(ax))
-	
